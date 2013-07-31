ae1713fb6577c12fcdf8478d95e9b86796ad8c3c
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2013-07-31 08:03:48 -0500

    remove tabs

--- cal.lua
+++ cal.lua
@@ -9,11 +9,11 @@
 --   + rename it to cal
 --
 -- 1. require it in your rc.lua
---	require("cal")
+-- require("cal")
 -- 2. attach the calendar to a widget of your choice (ex mytextclock)
---	cal.register(mytextclock)
+-- cal.register(mytextclock)
 --    If you don't like the default current day formating you can change it as following
---	cal.register(mytextclock, "<b>%s</b>") -- now the current day is bold instead of underlined
+-- cal.register(mytextclock, "<b>%s</b>") -- now the current day is bold instead of underlined
 --
 -- # How to Use #
 -- Just hover with your mouse over the widget, you register and the calendar popup.
@@ -31,23 +31,23 @@ local state = {}
 local current_day_format = "<u>%s</u>"
 
 function displayMonth(month,year,weekStart)
-	local t,wkSt=os.time{year=year, month=month+1, day=0},weekStart or 1
-	local d=os.date("*t",t)
-	local mthDays,stDay=d.day,(d.wday-d.day-wkSt+1)%7
+   local t,wkSt=os.time{year=year, month=month+1, day=0},weekStart or 1
+   local d=os.date("*t",t)
+   local mthDays,stDay=d.day,(d.wday-d.day-wkSt+1)%7
 
-	local lines = "    "
+   local lines = "    "
 
-	for x=0,6 do
-		lines = lines .. os.date("%a ",os.time{year=2006,month=1,day=x+wkSt})
-	end
+   for x=0,6 do
+      lines = lines .. os.date("%a ",os.time{year=2006,month=1,day=x+wkSt})
+   end
 
-	lines = lines .. "\n" .. os.date(" %V",os.time{year=year,month=month,day=1})
+   lines = lines .. "\n" .. os.date(" %V",os.time{year=year,month=month,day=1})
 
-	local writeLine = 1
-	while writeLine < (stDay + 1) do
-		lines = lines .. "    "
-		writeLine = writeLine + 1
-	end
+   local writeLine = 1
+   while writeLine < (stDay + 1) do
+      lines = lines .. "    "
+      writeLine = writeLine + 1
+   end
 
         for d=1,mthDays do
                 local x = d
@@ -73,55 +73,55 @@ function displayMonth(month,year,weekStart)
         end
         local header = os.date("%B %Y\n",os.time{year=year,month=month,day=1})
 
-	return header .. "\n" .. lines
+   return header .. "\n" .. lines
 end
 
 
 function register(mywidget, custom_current_day_format)
-	if custom_current_day_format then current_day_format = custom_current_day_format end
+   if custom_current_day_format then current_day_format = custom_current_day_format end
 
-	if not tooltip then
-		tooltip = awful.tooltip({})
+   if not tooltip then
+      tooltip = awful.tooltip({})
                 function tooltip:update()
                         local month, year = os.date('%m'), os.date('%Y')
                         state = {month, year}
                         tooltip:set_text(string.format('<span font_desc="monospace">%s</span>', displayMonth(month, year, 2)))
                 end
                 tooltip:update()
-	end
-	tooltip:add_to_object(mywidget)
+   end
+   tooltip:add_to_object(mywidget)
 
-	mywidget:add_signal("mouse::enter",tooltip.update)
+   mywidget:add_signal("mouse::enter",tooltip.update)
 
-	mywidget:buttons(awful.util.table.join(
-	awful.button({ }, 1, function()
-		switchMonth(-1)
-	end),
-	awful.button({ }, 3, function()
-		switchMonth(1)
-	end),
-	awful.button({ }, 4, function()
-		switchMonth(-1)
-	end),
-	awful.button({ }, 5, function()
-		switchMonth(1)
-	end),
-	awful.button({ 'Shift' }, 1, function()
-		switchMonth(-12)
-	end),
-	awful.button({ 'Shift' }, 3, function()
-		switchMonth(12)
-	end),
-	awful.button({ 'Shift' }, 4, function()
-		switchMonth(-12)
-	end),
-	awful.button({ 'Shift' }, 5, function()
-		switchMonth(12)
-	end)))
+   mywidget:buttons(awful.util.table.join(
+   awful.button({ }, 1, function()
+      switchMonth(-1)
+   end),
+   awful.button({ }, 3, function()
+      switchMonth(1)
+   end),
+   awful.button({ }, 4, function()
+      switchMonth(-1)
+   end),
+   awful.button({ }, 5, function()
+      switchMonth(1)
+   end),
+   awful.button({ 'Shift' }, 1, function()
+      switchMonth(-12)
+   end),
+   awful.button({ 'Shift' }, 3, function()
+      switchMonth(12)
+   end),
+   awful.button({ 'Shift' }, 4, function()
+      switchMonth(-12)
+   end),
+   awful.button({ 'Shift' }, 5, function()
+      switchMonth(12)
+   end)))
 end
 
 function switchMonth(delta)
-	state[1] = state[1] + (delta or 1)
-	local text = string.format('<span font_desc="monospace">%s</span>', displayMonth(state[1], state[2], 2))
-	tooltip:set_text(text)
+   state[1] = state[1] + (delta or 1)
+   local text = string.format('<span font_desc="monospace">%s</span>', displayMonth(state[1], state[2], 2))
+   tooltip:set_text(text)
 end
